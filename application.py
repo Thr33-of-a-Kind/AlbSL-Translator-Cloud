f6d88e7bcdaea0e28ccd4eb4c56737f1f9c879e5
Drin Karkini <drink@cdata.com>
2023-06-28 21:45:34 +0200

    Add a dropdown list for using all models

--- application.py
+++ application.py
@@ -1,16 +1,35 @@
+import os.path
+import pickle
+import warnings
+import xgboost
+import av
 import cv2
 import mediapipe as mp
 import numpy as np
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, WebRtcMode
-import pickle
-import warnings
-import av
-import os.path
 from twilio.rest import Client
 
 warnings.filterwarnings("ignore")
 st.set_option('deprecation.showfileUploaderEncoding', False)
+st.title("AlbSL Translator")
+
+
+@st.cache_data
+def format_labels(option):
+    option = str(option)
+    option = option.replace('_', ' ')
+    option = option.title()
+    return option
+
+
+model_names = ["decision_tree", "random_forest", "bagging", "naive_bayes", "svm", "logistic_regression", "knn",
+               "xgboost", "ridge"]
+model_option = st.selectbox(
+    'What model do you want to use ?',
+    model_names,
+    format_func=format_labels
+)
 
 absolute_path = os.path.dirname(__file__)
 
@@ -30,13 +49,19 @@ def load_labels():
 
 
 @st.cache_data
-def load_model():
-    modelPath = absolute_path + "/model.pkl"
-    return pickle.load(open(modelPath, 'rb'))['model']
+def load_models():
+    models = {}
+
+    for model_name in model_names:
+        model_path = absolute_path + '/' + model_name + '.pkl'
+        model = pickle.load(open(model_path, 'rb'))['model']
+        models[model_name] = model
+
+    return models
 
 
 labels = load_labels()
-model = load_model()
+models = load_models()
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=True, min_detection_confidence=0.3)
@@ -76,16 +101,18 @@ def callback(frame):
 
         dataAux = np.pad(dataAux, (0, 84 - len(dataAux)))
 
+        model = models[model_option]
+
         prediction = model.predict([np.asarray(dataAux)])
         predicted_character = labels[int(prediction[0])]
 
         cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 4)
         cv2.putText(image, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
 
-        return av.VideoFrame.from_ndarray(image, format="bgr24")
+    return av.VideoFrame.from_ndarray(image, format="bgr24")
 
 
-@st.cache_data  # type: ignore
+@st.cache_data
 def get_ice_servers():
     twilio_id = st.secrets["twilio_id"]
     twilio_key = st.secrets["twilio_key"]
@@ -99,7 +126,7 @@ webrtc_ctx = webrtc_streamer(
     key="AlbSL Translator",
     mode=WebRtcMode.SENDRECV,
     rtc_configuration={"iceServers": get_ice_servers()},
-    media_stream_constraints={"video": True, "audio": True},
+    media_stream_constraints={"video": True},
     video_frame_callback=callback,
     async_processing=True
 )
